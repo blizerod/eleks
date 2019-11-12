c6c1588214909b5d3ee00a22ed9f5f4dc385d924
Oleksandr <blizerod@gmail.com>
2019-11-12 23:12:15 +0200

    dev branch update

--- src/test/java/Reflection.java
+++ src/test/java/Reflection.java
@@ -19,6 +19,8 @@ public class Reflection {
         System.out.println("new String");
 
         System.out.println("String from rule branch");
+
+        System.out.println("Dev branch");
     }
 
 
