f11bfb5a4c63ac1593dd08ee090f9d9b690ecb8d
Oleksandr <blizerod@gmail.com>
2019-11-12 23:10:10 +0200

    after fix merging

--- src/test/java/Reflection.java
+++ src/test/java/Reflection.java
@@ -19,6 +19,8 @@ public class Reflection {
         System.out.println("String from rule branch");
         System.out.println("new String");
 
+        System.out.println("Third commit");
+
     }
 
 
